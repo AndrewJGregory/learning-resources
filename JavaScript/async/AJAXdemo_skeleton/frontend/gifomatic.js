505534c75e55258944f10f7602847d346202ef8f
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-11-15 08:30:20 -0500

    make instructions clearer

--- JavaScript/async/AJAXdemo_skeleton/frontend/gifomatic.js
+++ JavaScript/async/AJAXdemo_skeleton/frontend/gifomatic.js
@@ -42,6 +42,8 @@ const fetchNewGif = () => {
   const $input = $("#new-gif-query");
   const queryString = $input.val();
   $input.val("");
+
+  // TODO: Initiate request to giphy api endpoint, add gif to the the DOM
 };
 
 const saveGif = e => {
@@ -53,7 +55,7 @@ const saveGif = e => {
     url: $(".gif-display > img").attr("src"),
   };
 
-  // TODO: Initiate AJAX request to Rails backend, give a message if successful
+  // TODO: Initiate request to Rails backend, give a message if successful, otherwise display error message(s)
 };
 
 const fetchSavedGif = () => {
@@ -61,7 +63,7 @@ const fetchSavedGif = () => {
   const title = $input.val();
   $input.val("");
 
-  // TODO: Initate AJAX request to Rails backend, add gif to the DOM if successful
+  // TODO: Initate request to Rails backend, add gif to the DOM if successful. Otherwise, display error message(s)
 };
 
 // ------------- CALLBACK HELL ---------------
