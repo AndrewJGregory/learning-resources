7b663599ff540d6c58005321343320b1497f38f5
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-11-14 22:24:18 -0500

    add save gif for all ajax, xhr, fetch

--- JavaScript/async/AJAXdemo_solution/frontend/gif_api_util.js
+++ JavaScript/async/AJAXdemo_solution/frontend/gif_api_util.js
@@ -3,7 +3,6 @@ const appendGif = gifUrl => {
   $(".gif-display").append($(`<img class="gif" src=${gifUrl} />`));
 };
 
-
 const GifAPIUtil = {
   XMLHttpRequest: {
     getNewGif: queryString => {
@@ -14,19 +13,31 @@ const GifAPIUtil = {
           appendGif(url);
         }
       };
-      xhr.open("GET", `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`);
+      xhr.open(
+        "GET",
+        `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`,
+      );
       xhr.send();
     },
-      const req = new XMLHttpRequest();
-      req.addEventListener("load", e => {
-        const url = JSON.parse(e.currentTarget.response).data.image_url;
-        appendGif(url);
-      });
-      req.open("GET", `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`);
-      req.send();
+    saveGif: gif => {
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            $(".messages").html("successfully saved");
+          } else {
+            const errorMsgs = JSON.parse(xhr.response);
+            $(".messages").html(errorMsgs.join(", "));
+          }
+        }
+      };
+      xhr.open("POST", "/gifs");
+      const csrfToken = $('meta[name="csrf-token"]').attr("content");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("x-csrf-token", csrfToken);
+      xhr.send(JSON.stringify({ gif }));
     },
-    saveGif: gif => { },
-    getSavedGif: title => { }
+    getSavedGif: title => {},
   },
 
   AJAX: {
@@ -36,18 +47,58 @@ const GifAPIUtil = {
         url: `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`,
       });
     },
-    saveGif: gif => { },
-    getSavedGif: title => { },
+    saveGif: gif => {
+      return $.ajax({
+        method: "POST",
+        url: "/gifs",
+        data: { gif },
+        success: gif => {
+          $(".messages").html("successfully saved");
+        },
+        error: res => {
+          const errorMsgs = res.responseJSON;
+          $(".messages").html(errorMsgs.join(", "));
+        },
+      });
+    },
+    getSavedGif: title => {},
   },
 
   fetch: {
     newGifFetch: queryString => {
-      return fetch(`https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`)
+      return fetch(
+        `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`,
+      );
     },
-    saveGif: gif => { },
-    getSavedGif: title => { },
-  },
+    saveGif: gif => {
+      return fetch("/gifs", {
+        method: "POST",
+        body: JSON.stringify(gif),
+        headers: {
+          "Content-Type": "application/json",
+          "x-csrf-token": $('meta[name="csrf-token"]').attr("content"),
+        },
+      })
+        .then(res => {
+          if (!res.ok) {
+            throw res;
+          }
+          return res.json();
+        })
+        .then(
+          response => {
+            $(".messages").html("successfully saved");
+          },
+          err => {
+            err.json().then(errorMsgs => {
+              $(".messages").html(errorMsgs.join(", "));
+            });
+          },
+        );
+    },
 
+    getSavedGif: title => {},
+  },
 };
 
 module.exports = GifAPIUtil;
