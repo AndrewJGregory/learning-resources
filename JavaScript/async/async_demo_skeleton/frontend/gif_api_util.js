97b8ff1ee32bebc59495897653d000fc828faffa
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-11-16 15:00:53 -0500

    add giphy api endpoint

--- JavaScript/async/async_demo_skeleton/frontend/gif_api_util.js
+++ JavaScript/async/async_demo_skeleton/frontend/gif_api_util.js
@@ -3,6 +3,9 @@ const appendGif = gifUrl => {
   $(".gif-display").append($(`<img class="gif" src=${gifUrl} />`));
 };
 
+const makeUrl = queryString =>
+  `https://api.giphy.com/v1/gifs/random?tag=${queryString}&api_key=9IfxO6R6fpEZMAdqdw66QUgQdPejVIAW&rating=G`;
+
 const GifAPIUtil = {
   XMLHttpRequest: {
     getNewGif: queryString => {},
