4883a7b34db756aa16f93810f4f6fde25f20d1ca
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-07-01 19:18:30 -0400

    add promise solution

--- coding-challenges/asyncify/main.js
+++ coding-challenges/asyncify/main.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// step 1: callback
+
 const hasName = target => {
   fs.readFile("./data.csv", (err, buffer) => {
     const names = buffer
@@ -16,4 +18,26 @@ const hasName = target => {
   });
 };
 
-hasName("Andrew");
+// step 2: promises
+
+const hasNamePromise = target => {
+  readTheFile("./data.csv").then(res => {
+    const count = res.reduce((sum, name) => (name === target ? ++sum : sum), 0);
+    console.log(count);
+  });
+};
+
+const readTheFile = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, buffer) => {
+      if (err) reject(err);
+      const names = buffer
+        .toString()
+        .split("\n")
+        .slice(1)
+        .filter(str => str)
+        .map(str => JSON.parse(str.split(",")[1]));
+      resolve(names);
+    });
+  });
+};
