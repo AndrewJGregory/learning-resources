28fa3add3bebd92ced4eb80614c91ac650fe1abd
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-07-02 08:34:23 -0400

    handle err callback

--- coding-challenges/asyncify/main.js
+++ coding-challenges/asyncify/main.js
@@ -4,17 +4,21 @@ const fs = require("fs");
 
 const hasName = target => {
   fs.readFile("./data.csv", (err, buffer) => {
-    const names = buffer
-      .toString()
-      .split("\n")
-      .slice(1)
-      .filter(str => str)
-      .map(str => JSON.parse(str.split(",")[1]));
-    const count = names.reduce(
-      (sum, name) => (name === target ? ++sum : sum),
-      0
-    );
-    console.log(count);
+    if (err) {
+      console.log(err);
+    } else {
+      const names = buffer
+        .toString()
+        .split("\n")
+        .slice(1)
+        .filter(str => str)
+        .map(str => JSON.parse(str.split(",")[1]));
+      const count = names.reduce(
+        (sum, name) => (name === target ? ++sum : sum),
+        0
+      );
+      console.log(count);
+    }
   });
 };
 
@@ -66,4 +70,4 @@ const hasNameAsyncAwait = async target => {
   }
 };
 
-hasNameAsyncAwait("Andrew");
+hasName("Andrew");
